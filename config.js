ee47e662e284f1ba32f3882f6952585d828af0bf
hk28z8tvc7-cell <hk28z8tvc7@privaterelay.appleid.com>
2026-07-18 13:41:02 -0400

    Add Supabase browser config

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GYM_TRACKER_CONFIG = window.GYM_TRACKER_CONFIG || {
+  supabaseUrl: "https://vukisdktbflkfvtpjfeh.supabase.co",
+  supabaseAnonKey: "sb_publishable_36_CAs3X98bR7zB6r0wgrA_wx8P-zXk",
+};
