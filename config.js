ad0dc0a16752d76bfdbd424d8c0243532abb3207
hk28z8tvc7-cell <hk28z8tvc7@privaterelay.appleid.com>
2026-07-18 14:04:00 -0400

    Add hosted app URL for auth redirects

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.GYM_TRACKER_CONFIG = window.GYM_TRACKER_CONFIG || {
   supabaseUrl: "https://vukisdktbflkfvtpjfeh.supabase.co",
   supabaseAnonKey: "sb_publishable_36_CAs3X98bR7zB6r0wgrA_wx8P-zXk",
+  appUrl: "https://hk28z8tvc7-cell.github.io/gym-progression-tracker/",
 };
