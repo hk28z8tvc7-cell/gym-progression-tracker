d8df172e9a9c56bf33d76ec0a4dd592f6b5beef2
hk28z8tvc7-cell <hk28z8tvc7@privaterelay.appleid.com>
2026-07-18 13:43:58 -0400

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "gym-progression-tracker-v3";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./js/app-core.js?v=3",
+  "./js/app-auth.js?v=3",
+  "./js/app-data.js?v=3",
+  "./js/app-views.js?v=3",
+  "./js/app-events.js?v=3",
+  "./config.js",
+  "./manifest.json",
+  "./icons/icon-192.svg",
+  "./icons/icon-512.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/api/")) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
+});
